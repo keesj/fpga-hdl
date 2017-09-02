d7e5943e6966b50f202bfef88e6b10a0a686f3a3
keesj <kees.jongenburger@gmail.com>
2017-09-02 08:26:17 +0200

    Adding some makefile based examples for the papilio-pro.

---
+++ papilio-pro/blink/blink.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity blink is
+    Port ( clk : in  STD_LOGIC;
+           led : out  STD_LOGIC;
+			  sig : out  std_logic_vector(15 downto 0)
+			  );
+end blink;
+
+architecture Behavioral of blink is
+	signal counter : std_logic_vector(25 downto 0) := (others => '0');
+	alias fastCounter: std_logic_vector(15 downto 0) is counter(24 downto 9);
+begin
+        clk_proc: process(clk)
+        begin
+                if rising_edge(clk) then
+                        counter <= counter + '1';
+                        led <= counter(25);
+								sig <= fastCounter;
+                end if;
+        end process;
+end Behavioral;
+
---
+++ papilio-pro/user_led/user_led.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity user_led is
+    Port (
+           led : out  STD_LOGIC);
+end user_led;
+
+architecture Behavioral of user_led is
+
+begin
+    led <= '1';
+end Behavioral;
+
