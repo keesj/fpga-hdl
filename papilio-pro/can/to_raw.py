3ea35de7271761022297838ae5c75962a460011f
keesj <kees.jongenburger@gmail.com>
2018-03-14 21:21:21 +0100

    Add to raw script

---
+++ papilio-pro/can/to_raw.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import binascii
+
+f = open("can_tx_testbench_data_out.hex")
+raw = open("out","w")
+counter=0
+for line in f.readlines():
+	line = line.strip()
+	binval = int(line,16);
+	for i in bin(binval)[2:]:
+		counter = counter +1
+		if i == "1":
+			raw.write(chr(1 + 2 * (counter % 1)))
+		else:
+			raw.write(chr(0 + 2 * (counter % 1)))
+    		
