e1533ebd6ef0b4f69a2eac1998432db8e49a68f2
keesj <kees.jongenburger@gmail.com>
2018-04-11 19:38:14 +0200

    Use constants in can_wb

--- papilio-pro/can/syn/can_wb.vhd
+++ papilio-pro/can/syn/can_wb.vhd
@@ -21,7 +21,7 @@ architecture behavior of can_wb is
   signal  wb_clk_i:    std_logic;                     -- Wishbone clock
   signal  wb_rst_i:    std_logic;                     -- Wishbone reset (synchronous)
   signal  wb_dat_i:    std_logic_vector(31 downto 0); -- Wishbone data input  (32 bits)
-  signal  wb_adr_i:    std_logic_vector(24 downto 0); -- Wishbone address input  (32 bits)
+  signal  wb_adr_i:    std_logic_vector(31 downto 0); -- Wishbone address input  (32 bits)
   signal  wb_we_i:     std_logic;                     -- Wishbone write enable signal
   signal  wb_cyc_i:    std_logic;                     -- Wishbone cycle signal
   signal  wb_stb_i:    std_logic;                     -- Wishbone strobe signal  
@@ -51,13 +51,53 @@ architecture behavior of can_wb is
   signal can0_phy_tx_en             : std_logic;
   signal can0_phy_rx                : std_logic;
 
+    -- 00 RO version h"13371337'
+  -- 01 RO status bit 0 denotes a transmit request. bit 1 a read to receive singal
+  -- 02 RW CONF  (loopback,selftest )
+
+  -- 03 RW CONF sample rate
+  -- 04 RW CONF RX id filter
+  -- 05 RW CONF RX id filter mask
+
+  -- 06 RW TX tx_id (11 msb are id) and lsb is request response
+  -- 07 RW TX the 4 lsb bytes are the data length (code)
+  -- 08 RW TX bits 31 to 0 of the data 
+  -- 09 RW bits 63 to 32 of the data 
+  -- 0A WO TX_VALID
+
+  -- 0B RO RX tx _id (11 msb are id) and lsb is request response
+  -- 0C RO RX LCD the 4 lsb bytes are the data length (code)
+  -- 0d RO RX RX_DAT0 
+  -- 0e RO RX RX_DAT1
+  -- 0f WO TX_DATA_READ_READY (the data has been read)
+
+  constant REG_VERSION        : std_logic_vector(31 downto 0) := x"00000000";
+  constant REG_STATUS         : std_logic_vector(31 downto 0) := x"00000001";
+  constant REG_CONF           : std_logic_vector(31 downto 0) := x"00000002";
+  constant REG_SAMPLE_RATE    : std_logic_vector(31 downto 0) := x"00000003";
+  constant REG_ID_FILTER      : std_logic_vector(31 downto 0) := x"00000004";
+  constant REG_ID_FILTER_MASK : std_logic_vector(31 downto 0) := x"00000005";
+
+  constant REG_TX_ID          : std_logic_vector(31 downto 0) := x"00000006";
+  constant REG_TX_DLC         : std_logic_vector(31 downto 0) := x"00000007";
+  constant REG_TX_DATA0       : std_logic_vector(31 downto 0) := x"00000008";
+  constant REG_TX_DATA1       : std_logic_vector(31 downto 0) := x"00000009";
+  constant REG_TX_VALID       : std_logic_vector(31 downto 0) := x"0000000a";
+
+  constant REG_RX_ID          : std_logic_vector(31 downto 0) := x"0000000b";
+  constant REG_RX_DLC         : std_logic_vector(31 downto 0) := x"0000000c";
+  constant REG_RX_DATA0       : std_logic_vector(31 downto 0) := x"0000000d";
+  constant REG_RX_DATA1       : std_logic_vector(31 downto 0) := x"0000000e";
+  constant REG_RX_DRR         : std_logic_vector(31 downto 0) := x"0000000f";
+
+
 begin
 
   -- Unpack the wishbone array into signals so the modules code is not confusing. - Don't touch.
   wb_clk_i <= wishbone_in(61);           -- clock
   wb_rst_i <= wishbone_in(60);           -- reset signal
   wb_dat_i <= wishbone_in(59 downto 28); -- the date the master wishes to write
-  wb_adr_i <= wishbone_in(27 downto 3);  -- contains the address of the request
+  wb_adr_i <= "0000000" & wishbone_in(27 downto 3);  -- contains the address of the request
   wb_we_i  <= wishbone_in(2);             -- true for any write requests
   wb_cyc_i <= wishbone_in(1);            -- is true any time a wishbone transaction is taking place
   wb_stb_i <= wishbone_in(0);            -- is true for any bus transaction request.
@@ -122,23 +162,24 @@ begin
   process(wb_clk_i)
   begin
     if rising_edge(wb_clk_i) then  -- Synchronous to the rising edge of the clock
-      case wb_adr_i(7 downto 0) is
-        when x"00" => wb_dat_o <= version;
-        when x"01" => wb_dat_o <= can0_can_status ;
-        when x"02" => wb_dat_o <= can0_config;
-        when x"03" => wb_dat_o <= can0_can_sample_rate;
-        when x"04" => wb_dat_o <= can0_can_rx_id_filter;
-        when x"05" => wb_dat_o <= can0_can_rx_id_filter_mask;
-        when x"06" => wb_dat_o <= can0_can_tx_id;                -- not very usefull
-        when x"07" => wb_dat_o <= (31 downto 4 => '0')  & can0_can_tx_dlc;               -- not very usefull
-        when x"08" => wb_dat_o <= can0_can_tx_data(31 downto 0); -- not very usefull
-        when x"09" => wb_dat_o <= can0_can_tx_data(63 downto 32);-- not very usefull
-        when x"0a" => wb_dat_o <=  (31 downto 1 => '0') & can0_can_tx_valid;             -- not very usefull
-        when x"0b" => wb_dat_o <= can0_can_rx_id;
-        when x"0c" => wb_dat_o <= (31 downto 4 => '0') & can0_can_rx_dlc;
-        when x"0d" => wb_dat_o <= can0_can_rx_data(31 downto 0);
-        when x"0e" => wb_dat_o <= can0_can_rx_data(63 downto 32);
-        when x"0f" => -- ksip wb_dat_o <= allzero(31 downto 1) & can0_can_rx_drr;
+      case wb_adr_i is
+        when REG_VERSION => wb_dat_o <= version;
+        when REG_STATUS => wb_dat_o <= can0_can_status ;
+        when REG_CONF => wb_dat_o <= can0_config;
+        when REG_SAMPLE_RATE => wb_dat_o <= can0_can_sample_rate;
+        when REG_ID_FILTER => wb_dat_o <= can0_can_rx_id_filter;
+        when REG_ID_FILTER_MASK => wb_dat_o <= can0_can_rx_id_filter_mask;
+        when REG_TX_ID => wb_dat_o <= can0_can_tx_id;                -- not very usefull
+        when REG_TX_DLC => wb_dat_o <= (31 downto 4 => '0')  & can0_can_tx_dlc;               -- not very usefull
+        when REG_TX_DATA0 => wb_dat_o <= can0_can_tx_data(31 downto 0); -- not very usefull
+        when REG_TX_DATA1 => wb_dat_o <= can0_can_tx_data(63 downto 32);-- not very usefull
+        when REG_TX_VALID => wb_dat_o <=  (31 downto 1 => '0') & can0_can_tx_valid;             -- not very usefull
+
+        when REG_RX_ID => wb_dat_o <= can0_can_rx_id;
+        when REG_RX_DLC => wb_dat_o <= (31 downto 4 => '0') & can0_can_rx_dlc;
+        when REG_RX_DATA0 => wb_dat_o <= can0_can_rx_data(31 downto 0);
+        when REG_RX_DATA1 => wb_dat_o <= can0_can_rx_data(63 downto 32);
+        when REG_RX_DRR => -- ksip wb_dat_o <= allzero(31 downto 1) & can0_can_rx_drr;
         when others => wb_dat_o <= (others => 'X'); -- Return undefined for all other addresses
       end case;
     end if;
@@ -149,24 +190,24 @@ begin
   begin
     if rising_edge(wb_clk_i) then  -- Synchronous to the rising edge of the clock
       if wb_cyc_i='1' and wb_stb_i='1' and wb_we_i='1' then
-        case wb_adr_i(7 downto 0) is
+        case wb_adr_i is
           -- configuration fields
-          when x"00" =>  --ignore version
-          when x"01" => -- ignore can0_can_status <=  wb_dat_i;
-          when x"02" => can0_config <=  wb_dat_i;
-          when x"03" => can0_can_sample_rate <=  wb_dat_i;
-          when x"04" => can0_can_rx_id_filter <=  wb_dat_i;
-          when x"05" => can0_can_rx_id_filter_mask <=  wb_dat_i;
-          when x"06" => can0_can_tx_id <=  wb_dat_i;
-          when x"07" => can0_can_tx_dlc <=  wb_dat_i(3 downto 0);
-          when x"08" => can0_can_tx_data(31 downto 0) <=  wb_dat_i;
-          when x"09" => can0_can_tx_data(63 downto 32) <=  wb_dat_i;
-          when x"0a" => can0_can_tx_valid <= wb_dat_i(0);
-          when x"0b" =>  -- skip can0_can_rx_id <=  wb_dat_i;
-          when x"0c" =>  -- skip can0_can_rx_dlc <=  wb_dat_i;
-          when x"0d" =>  -- skip  can0_can_rx_data(31 downto 0) <=  wb_dat_i;
-          when x"0e" =>  -- skip can0_can_rx_data(63 downto 32) <=  wb_dat_i;
-          when x"0f" => can0_can_rx_drr <=  wb_dat_i(0);
+          when REG_VERSION =>  --ignore version
+          when REG_STATUS => -- ignore can0_can_status <=  wb_dat_i;
+          when REG_CONF => can0_config <=  wb_dat_i;
+          when REG_SAMPLE_RATE => can0_can_sample_rate <=  wb_dat_i;
+          when REG_ID_FILTER => can0_can_rx_id_filter <=  wb_dat_i;
+          when REG_ID_FILTER_MASK => can0_can_rx_id_filter_mask <=  wb_dat_i;
+          when REG_TX_ID => can0_can_tx_id <=  wb_dat_i;
+          when REG_TX_DLC => can0_can_tx_dlc <=  wb_dat_i(3 downto 0);
+          when REG_TX_DATA0 => can0_can_tx_data(31 downto 0) <=  wb_dat_i;
+          when REG_TX_DATA1 => can0_can_tx_data(63 downto 32) <=  wb_dat_i;
+          when REG_TX_VALID => can0_can_tx_valid <= wb_dat_i(0);
+          when REG_RX_ID =>  -- skip can0_can_rx_id <=  wb_dat_i;
+          when REG_RX_DLC =>  -- skip can0_can_rx_dlc <=  wb_dat_i;
+          when REG_RX_DATA0 =>  -- skip  can0_can_rx_data(31 downto 0) <=  wb_dat_i;
+          when REG_RX_DATA1 =>  -- skip can0_can_rx_data(63 downto 32) <=  wb_dat_i;
+          when REG_RX_DRR => can0_can_rx_drr <=  wb_dat_i(0);
           when others => 
         end case;
       end if;
